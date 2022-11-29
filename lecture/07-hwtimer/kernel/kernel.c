3fcd6ea2eff63cb0caeb97da0dc0d0285a002bb2
minired2154 <jordan1415926535@gmail.com>
2022-11-29 21:28:44 +0800

    add lecture 07-hwtimer

---
+++ lecture/07-hwtimer/kernel/kernel.c
@@ -0,0 +1,44 @@
+#include "os.h"
+
+/*
+ * Following functions SHOULD be called ONLY ONE time here,
+ * so just declared here ONCE and NOT included in file os.h.
+ */
+extern void uart_init(void);
+extern void page_init(void);
+extern void sched_init(void);
+extern void schedule(void);
+extern void os_main(void);
+extern void trap_init(void);
+extern void plic_init(void);
+extern void timer_init(void);
+
+
+void start_kernel(void) {
+    uart_init();
+    uart_puts("Hello, RVOS!\n");
+
+    page_init();
+
+    trap_init();
+
+    plic_init();
+    
+    timer_init();
+    
+    sched_init();
+
+    os_main();
+    while (1) {
+        schedule();
+    }
+
+    printf("do not show here\n");
+    int *a = malloc(10);
+    printf("malloc addr: %x\n", a);
+
+    while (1) {
+        int a = uart_getc();
+        uart_puts((char*) &a);
+    }; // stop here!
+}
---
+++ lecture/07-hwtimer/lib/user.c
@@ -0,0 +1,38 @@
+#include "os.h"
+
+#define DELAY 2000
+
+extern void trap_test(void);
+
+void user_task0(void)
+{
+	uart_puts("Task 0: Created!\n");
+	while (1) {
+		uart_puts("Task 0: Running...\n");
+		// trap_test();
+		task_delay(DELAY);
+		task_yield();
+	}
+}
+
+void user_task1(void)
+{
+	// int c = 0;
+	uart_puts("Task 1: Created!\n");
+	while (1) {
+		// uart_puts("Task 1: Running...\n");
+		task_delay(DELAY);
+		task_yield();
+		// if (++c > 3){
+		// 	task_exit();
+		// }
+	}
+}
+
+/* NOTICE: DON'T LOOP INFINITELY IN main() */
+void os_main(void)
+{
+	task_create(user_task0, NULL, 100);
+	task_create(user_task1, NULL, 101);
+}
+
